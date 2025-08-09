b5547429c2a36bed8fc7fed732f4e3e487b293ed
aaronmull <aaronmull421@gmail.com>
2025-08-09 10:53:37 -0400

    Auth pages

--- mobile/assets/styles/auth.styles.js
+++ mobile/assets/styles/auth.styles.js
@@ -11,9 +11,9 @@ export const styles = StyleSheet.create({
     justifyContent: "center",
   },
   illustration: {
-    width: "100%",
-    height: 310,
-    resizeMode: "contain",
+    width: 400,
+    height: 100,
+    contentFit: "contain",
   },
   title: {
     fontSize: 32,
